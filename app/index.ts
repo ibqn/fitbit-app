1ca77106f769464e5c9dd094ccea5c151a29f583
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2019-08-09 12:55:22 +0200

    add screenshot

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+import clock from 'clock';
+import document from 'document';
+
+clock.granularity = 'seconds';
+
+let clockID = document.getElementById('clockID');
+
+clock.ontick = (evt) => {
+    let hours = ('0' + evt.date.getHours()).slice(-2);
+    let mins = evt.date.getMinutes();
+    let secs = ('0' + evt.date.getSeconds()).slice(-2);
+
+    clockID.text = `${hours}:${mins}:${secs}`;
+    //console.log(evt.date.toString());
+}
--- readme.md
+++ readme.md
@@ -1,10 +1,12 @@
 # Fitbit application
 
+![screenshot](screenshot.png)
+
 The app was created with
 ```shell
 npx create-fitbit-app fitbit-app
 ```
-
+and `typescript` support was addred with
 ```shell
 npx fitbit-sdk-types apply
 ```
