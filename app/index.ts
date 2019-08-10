fe454b7a7e3c4b5489fe9e68eb367ad6db94f452
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2019-08-10 11:45:32 +0200

    move padZero to common module

--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,16 @@
-import clock from 'clock';
+import clock, { TickEvent } from 'clock';
 import document from 'document';
+
+import { zeroPad } from '../common/utils';
 
 clock.granularity = 'seconds';
 
-let clockID = document.getElementById('clockID');
+let clockID = document.getElementById('clockID') as Element;
 
-clock.ontick = (evt) => {
-    let hours = ('0' + evt.date.getHours()).slice(-2);
-    let mins = evt.date.getMinutes();
-    let secs = ('0' + evt.date.getSeconds()).slice(-2);
+clock.ontick = (evt: TickEvent) => {
+    let hours = zeroPad(evt.date.getHours());
+    let mins = zeroPad(evt.date.getMinutes());
+    let secs = zeroPad(evt.date.getSeconds());
 
     clockID.text = `${hours}:${mins}:${secs}`;
     //console.log(evt.date.toString());
---
+++ common/utils.ts
@@ -0,0 +1,4 @@
+
+export function zeroPad(num: number) {
+    return `0${num}`.slice(-2);
+}
