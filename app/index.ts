a302a223578f1c1f1beeebf7441bd41e90a16723
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2019-08-15 13:08:29 +0200

    add analog clocks

--- app/index.ts
+++ app/index.ts
@@ -5,13 +5,37 @@ import { zeroPad } from '../common/utils';
 
 clock.granularity = 'seconds';
 
-let clockID = document.getElementById('clockID') as Element;
+let hourHand = document.getElementById("hours") as GroupElement;
+let minHand = document.getElementById("mins") as GroupElement;
+let secHand = document.getElementById("secs") as GroupElement;
 
-clock.ontick = (evt: TickEvent) => {
-    let hours = zeroPad(evt.date.getHours());
-    let mins = zeroPad(evt.date.getMinutes());
-    let secs = zeroPad(evt.date.getSeconds());
-
-    clockID.text = `${hours}:${mins}:${secs}`;
-    //console.log(evt.date.toString());
+// Returns an angle (0-360) for the current hour in the day, including minutes
+function hoursToAngle(hours: number, minutes: number) {
+    let hourAngle = (360 / 12) * hours;
+    let minAngle = (360 / 12 / 60) * minutes;
+    return hourAngle + minAngle;
 }
+
+// Returns an angle (0-360) for minutes
+function minutesToAngle(minutes: number) {
+    return (360 / 60) * minutes;
+}
+
+// Returns an angle (0-360) for seconds
+function secondsToAngle(seconds: number) {
+    return (360 / 60) * seconds;
+}
+
+// Rotate the hands every tick
+function updateClock(today: Date) {
+    let hours = today.getHours() % 12;
+    let mins = today.getMinutes();
+    let secs = today.getSeconds();
+
+    (hourHand.groupTransform as Transform).rotate.angle = hoursToAngle(hours, mins);
+    (minHand.groupTransform as Transform).rotate.angle = minutesToAngle(mins);
+    (secHand.groupTransform as Transform).rotate.angle = secondsToAngle(secs);
+}
+
+// Update the clock every tick event
+clock.ontick = (evt: TickEvent) => updateClock(evt.date);
